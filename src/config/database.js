1cad4673c4d12535ec4ed0dcb7ed209da4e4f0e6
Shivam barthwal <shivam.paulbarthwal@gmail.com>
2025-08-12 13:59:30 +0530

    Updated Database
    the password is changed.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
-const ConnectDb = async () => {
-  await mongoose.connect(
-    "mongodb+srv://shivambarthwal:PGeuwtG7827Dz76P@namastenode.2ynkm5m.mongodb.net/devTinder"
-  );
-};
-
+async function ConnectDb() {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://shivambarthwal:AaxgJvl1U1WypDBn@namastenode.2ynkm5m.mongodb.net/?retryWrites=true&w=majority&appName=NamasteNode",
+      {
+        dbName: "devTinder",
+      }
+    );
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+}
 module.exports = ConnectDb;
